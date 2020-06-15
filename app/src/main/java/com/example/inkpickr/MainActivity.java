c1c4f4f969604a139f291e09e110b655d76d14df
Brady Hill <bradyhill86@yahoo.com>
2020-06-14 21:59:24 -0500

    Added Button and Text View Emulator continues to crash but code is sound ?

--- app/src/main/java/com/example/inkpickr/MainActivity.java
+++ app/src/main/java/com/example/inkpickr/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.inkpickr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int[] tattoos = {0,1,2,3,4,5,6,7};
+
+        Button randomButton = (Button) findViewById(R.id.randomButton);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView randomTattoo = (TextView) findViewById((R.id.randomTattoo));
+                Random random = new Random();
+                int randTat = random.nextInt(tattoos.length);
+                randomTattoo.setText(tattoos[randTat]);
+            }
+        });
     }
 }

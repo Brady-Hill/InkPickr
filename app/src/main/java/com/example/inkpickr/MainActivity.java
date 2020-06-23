43f45c0b59b83789b2f369bff4b32d28fe5bee4f
Brady Hill <bradyhill86@yahoo.com>
2020-06-23 12:46:45 -0500

    Removed Text View and Replaced with Image View

--- app/src/main/java/com/example/inkpickr/MainActivity.java
+++ app/src/main/java/com/example/inkpickr/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.inkpickr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -16,17 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String a = "A"; String b = "B"; String c = "C"; String d = "D";
-        final String[]tattoos = {a,b,c,d};
+        final Drawable[] tattoos = new Drawable[5];
+        Drawable a = getDrawable(R.drawable.tattoo1); tattoos[0] = a;
+        Drawable b = getDrawable(R.drawable.tattoo2); tattoos[1] = b;
+        Drawable c = getDrawable(R.drawable.tattoo3); tattoos[2] = c;
+        Drawable d = getDrawable(R.drawable.tattoo4); tattoos[3] = d;
+        Drawable e = getDrawable(R.drawable.tattoo5); tattoos[4] = e;
 
         Button randomButton = (Button) findViewById(R.id.randomButton);
         randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView randomTattoo = (TextView) findViewById((R.id.randomTattoo));
+                ImageView randomTattoo = (ImageView) findViewById((R.id.randomTattoo));
                 Random random = new Random();
                 int randTat = random.nextInt(tattoos.length);
-                randomTattoo.setText(tattoos[randTat]);
+                randomTattoo.setImageDrawable(tattoos[randTat]);
             }
         });
     }

5c0c14c7b0f8ca45a859d4768e27aeb43d3eb758
Brady Hill <bradyhill86@yahoo.com>
2020-06-28 15:26:51 -0500

    Added Spinner to Select From Different Types of Tattoos. Also made various UI Improvements

--- app/src/main/java/com/example/inkpickr/MainActivity.java
+++ app/src/main/java/com/example/inkpickr/MainActivity.java
@@ -6,26 +6,49 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Drawable[] tattoos;
+
+    final Drawable[] allTattoos = new Drawable[5];
+
+    final Drawable[] tribal = new Drawable[2];
+
+    final Drawable[] flash = new Drawable[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Drawable[] tattoos = new Drawable[5];
-        Drawable a = getDrawable(R.drawable.tattoo1); tattoos[0] = a;
-        Drawable b = getDrawable(R.drawable.tattoo2); tattoos[1] = b;
-        Drawable c = getDrawable(R.drawable.tattoo3); tattoos[2] = c;
-        Drawable d = getDrawable(R.drawable.tattoo4); tattoos[3] = d;
-        Drawable e = getDrawable(R.drawable.tattoo5); tattoos[4] = e;
+        //Set Up Spinner
+        Spinner spinner = findViewById(R.id.spinnerTattoo);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.tattooSpinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
+        //Populate Arrays
+        Drawable a = getDrawable(R.drawable.tattoo1); allTattoos[0] = a;
+        Drawable b = getDrawable(R.drawable.tattoo2); allTattoos[1] = b;
+        Drawable c = getDrawable(R.drawable.tattoo3); allTattoos[2] = c;
+        Drawable d = getDrawable(R.drawable.tattoo4); allTattoos[3] = d;
+        Drawable e = getDrawable(R.drawable.tattoo5); allTattoos[4] = e;
+
+        tribal[0] = d; tribal[1] = e;
+
+        flash[0] = a; flash[1] = b; flash[2] = c;
+
+        //Set up Button Click
         Button randomButton = (Button) findViewById(R.id.randomButton);
         randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +59,22 @@ public class MainActivity extends AppCompatActivity {
                 randomTattoo.setImageDrawable(tattoos[randTat]);
             }
         });
+    }
+    //Set up Item Selection For Spinner
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch(position){
+            case 0: tattoos = allTattoos;
+            break;
+            case 1: tattoos = tribal;
+            break;
+            case 2: tattoos = flash;
+            break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        tattoos = allTattoos;
     }
 }
